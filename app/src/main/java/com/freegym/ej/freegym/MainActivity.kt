ec0003f91414a29b5bd29d499dd443df5e6137d9
StanleySathler <stanleysathlerpinto@gmail.com>
2018-05-23 00:33:53 -0300

    Added Bottom Navigation in MainActivity
    The Bottom Navigation, according to the planned design, consists on the app's main navigation. It leads to Equipments, Stretchings and Gyms activities.

--- app/src/main/java/com/freegym/ej/freegym/MainActivity.kt
+++ app/src/main/java/com/freegym/ej/freegym/MainActivity.kt
@@ -6,9 +6,11 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
@@ -17,19 +19,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val nearestGym = findViewById<Button>(R.id.nearestGym)
 
-        checkLocationPermission()
+        val bottomNavigationView = findViewById<View>(R.id.bottom_navigation) as BottomNavigationView
+        bottomNavigationView.setOnNavigationItemSelectedListener { selectedItem ->
+            when (selectedItem.itemId) {
+                R.id.ic_equipment ->
+                    handleIconEquipmentTap()
+                R.id.ic_stretching ->
+                    handleIconStretchingTap()
+                R.id.ic_map ->
+                    handleIconMapTap()
+            }
 
-        nearestGym.setOnClickListener {
-            checkLocationPermission()
-
-            var intent = Intent(this, GymMapsActivity::class.java)
-            startActivity(intent)
-        };
+            true
+        }
     }
 
     private val MY_PERMISSIONS_REQUEST_LOCATION = 99
+
+    private fun handleIconMapTap() {
+        checkLocationPermission()
+        startActivity(Intent(this, GymMapsActivity::class.java))
+    }
+
+    private fun handleIconStretchingTap() {
+        Toast.makeText(application, "Alongamentos", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun handleIconEquipmentTap() {
+        Toast.makeText(application, "Equipamentos", Toast.LENGTH_SHORT).show()
+    }
 
     private fun checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
