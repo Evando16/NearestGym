622e1a5c568d89eab2a82046a52056f8e08732f7
StanleySathler <stanleysathlerpinto@gmail.com>
2018-05-30 02:12:23 -0300

    Basic EquipmentsActivity's refactor
    Renamed some methods and created some others to improve code readability.

--- app/src/main/java/com/freegym/ej/freegym/EquipmentsActivity.kt
+++ app/src/main/java/com/freegym/ej/freegym/EquipmentsActivity.kt
@@ -6,12 +6,10 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AlertDialog
 import android.support.v7.widget.StaggeredGridLayoutManager
-import android.view.View
 import android.widget.Toast
 import com.freegym.ej.freegym.adapter.EquipmentsAdapter
 import com.freegym.ej.freegym.model.Equipment
@@ -19,15 +17,24 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class EquipmentsActivity : AppCompatActivity() {
 
+    private val MY_PERMISSIONS_REQUEST_LOCATION = 99
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val recyclerView = equipments_recycler_view
-        recyclerView.adapter = EquipmentsAdapter(buildEquipments(), this)
-        recyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        initEquipmentsListRecyclerView()
+        initBottomNavigationView()
+    }
 
-        val bottomNavigationView = findViewById<View>(R.id.bottom_navigation) as BottomNavigationView
+    private fun initEquipmentsListRecyclerView() {
+        val equipmentsListRecyclerView = equipments_recycler_view
+        equipmentsListRecyclerView.adapter = EquipmentsAdapter(buildEquipments(), this)
+        equipmentsListRecyclerView.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+    }
+
+    private fun initBottomNavigationView() {
+        val bottomNavigationView = bottom_navigation
         bottomNavigationView.setOnNavigationItemSelectedListener { selectedItem ->
             when (selectedItem.itemId) {
                 R.id.ic_equipment ->
@@ -44,24 +51,23 @@ class EquipmentsActivity : AppCompatActivity() {
 
     private fun buildEquipments(): List<Equipment> {
         return listOf(
-                Equipment("Equipamento #1", ""),
-                Equipment("Equipamento #2", ""),
-                Equipment("Equipamento #3", ""),
-                Equipment("Equipamento #4", ""),
-                Equipment("Equipamento #5", ""),
-                Equipment("Equipamento #6", ""),
-                Equipment("Equipamento #7", ""),
-                Equipment("Equipamento #8", ""),
-                Equipment("Equipamento #9", ""),
-                Equipment("Equipamento #10", "")
+                Equipment("Multiexercitador", ""),
+                Equipment("Pressão de Pernas", ""),
+                Equipment("Remada Sentada", ""),
+                Equipment("Extensora e Agachamento", ""),
+                Equipment("Alongador", ""),
+                Equipment("Rotação Vertical", ""),
+                Equipment("Simulador de Cavalgada", ""),
+                Equipment("Rotação Dupla Digonal", ""),
+                Equipment("Simulador de Caminhada", ""),
+                Equipment("Esqui", "")
         )
     }
 
-    private val MY_PERMISSIONS_REQUEST_LOCATION = 99
-
     private fun handleIconMapTap() {
-        checkLocationPermission()
-        startActivity(Intent(this, GymMapsActivity::class.java))
+        ensureGetLocationPermission()
+        Toast.makeText(application, "Academias", Toast.LENGTH_SHORT).show()
+        //startActivity(Intent(this, GymMapsActivity::class.java))
     }
 
     private fun handleIconStretchingTap() {
@@ -72,7 +78,7 @@ class EquipmentsActivity : AppCompatActivity() {
         Toast.makeText(application, "Equipamentos", Toast.LENGTH_SHORT).show()
     }
 
-    private fun checkLocationPermission() {
+    private fun ensureGetLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                             Manifest.permission.ACCESS_FINE_LOCATION)) {
