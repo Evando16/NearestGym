3fe3f1032724c6148b88c2480d382a02881c30c3
StanleySathler <stanleysathlerpinto@gmail.com>
2018-06-12 02:23:25 -0300

    Added & configured Firebase to the project.
    Why using JSON if we can use Firebase? Easy to install, configure & start using it.

--- app/src/main/java/com/freegym/ej/freegym/EquipmentsActivity.kt
+++ app/src/main/java/com/freegym/ej/freegym/EquipmentsActivity.kt
@@ -11,6 +11,10 @@ import android.support.v7.app.AlertDialog
 import android.widget.Toast
 import com.freegym.ej.freegym.fragment.EquipmentsFragment
 import com.freegym.ej.freegym.fragment.StretchingsFragment
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.activity_equipments.*
 
 class EquipmentsActivity : AppCompatActivity() {
@@ -24,7 +28,7 @@ class EquipmentsActivity : AppCompatActivity() {
 
         initBottomNavigationView()
         initFragment()
-        loadDatabase()
+        addDatabaseUpdateListener()
     }
 
     private fun initBottomNavigationView() {
@@ -47,8 +51,20 @@ class EquipmentsActivity : AppCompatActivity() {
         handleIconEquipmentTap()
     }
 
-    private fun loadDatabase() {
-        // Should load initial data from database
+    private fun addDatabaseUpdateListener() {
+        val dbReference = FirebaseDatabase.getInstance().reference
+
+        /*
+         * To do: We don't need to use `addValueEventListener`. `addListenerForSingleValueEvent`
+         * is way better, since we don't need to listen to changes all the time. But for development
+         * purposes, `addValueEventListener` saves us a lot of time.
+         */
+        dbReference.child("equipments").addValueEventListener(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                println("Received data: ${snapshot.value}")
+            }
+            override fun onCancelled(error: DatabaseError) {}
+        })
     }
 
     private fun handleIconEquipmentTap() {
